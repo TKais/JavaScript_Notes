f5bbd6cc81c07f93fa4c3d65899d0987adde9b8c
TKais <TiffanyTKaiser@gmail.com>
2014-11-02 21:42:00 -0800

    More notes on object constructors

--- objects.js
+++ objects.js
@@ -1,7 +1,9 @@
 An object is an unordered list of primitive data (and sometimes reference data types) types that are stored as name-value pairs
 
+---------------------------------------------------------------------------------
 
 Object Literals
+
 // This is an empty object initialized using the object literal notation​
 ​var myBooks = {};
 ​
@@ -16,6 +18,40 @@ console.log("Hmm Hmm Good");
 }
 }
 
+-------------------------------------------------------------------------------------
 
 Object Constructor
+
  The second most common way to create objects is with Object constructor. A constructor is a function used for initializing new objects, and you use the new keyword to call the constructor.
+var mango =  new Object ();
+mango.color = "yellow";
+mango.shape= "round";
+mango.sweetness = 8;
+​
+mango.howSweetAmI = function () {
+console.log("Hmm Hmm Good");
+}
+
+---------------------------------------------------------------------------------------
+
+Constructor Pattern for Creating Objects
+
+function Fruit (theColor, theSweetness, theFruitName, theNativeToLand) {
+​
+    this.color = theColor;
+    this.sweetness = theSweetness;
+    this.fruitName = theFruitName;
+    this.nativeToLand = theNativeToLand;
+​
+    this.showName = function () {
+        console.log("This is a " + this.fruitName);
+    }
+​
+    this.nativeTo = function () {
+    this.nativeToLand.forEach(function (eachCountry)  {
+       console.log("Grown in:" + eachCountry);
+        });
+    }
+​
+​
+}
