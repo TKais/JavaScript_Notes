9d3c2d382591dc9c38bfd8875c8497dc9e1cb636
TKais <TiffanyTKaiser@gmail.com>
2014-11-07 09:45:15 -0800

    Example of prototype property

--- prototypes.js
+++ prototypes.js
@@ -5,4 +5,15 @@ TWO MAIN CONCEPTS IN PROTOTYPES
    -Its empty by default
    -Attach properties and methods to it when you want to implement inheritence
    -Not enumerable (meaning it will not be logged out in a for/in loop)
-   -Used primarily for inheritence
+   -Used primarily for inheritence: you add methods and properties on a function’s prototype property to make those methods and properties available to instances of that function.
+
+   EX)
+
+   function PrintStuff(myDocuments){
+    this.documents = myDocuments;
+   }
+
+  //Add print() method to PrintStuff prototype property so that other instances (objects) can inherit it
+   PrintStuff.prototype.print = function(){
+    console.log(this.documents);
+   }
