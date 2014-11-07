fb125fd4d01ba08caedf44d707a6ab6afaddd9e4
TKais <TiffanyTKaiser@gmail.com>
2014-11-06 17:51:57 -0800

    Moving on to prototypes in depth

---
+++ prototypes.js
@@ -0,0 +1,8 @@
+TWO MAIN CONCEPTS IN PROTOTYPES
+
+1) Prototype Property = 
+   -Every JavaScript function has it
+   -Its empty by default
+   -Attach properties and methods to it when you want to implement inheritence
+   -Not enumerable (meaning it will not be logged out in a for/in loop)
+   -Used primarily for inheritence
