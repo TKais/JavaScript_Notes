6450d0324de2190025dd5dd38837aebb55bc93f6
TKais <TiffanyTKaiser@gmail.com>
2014-11-08 15:01:44 -0800

    Definition and two kinds of variable scope

---
+++ scopeAndHoisting.js
@@ -0,0 +1,10 @@
+VARIABLE SCOPE
+
+A variables scope is the context in which it exists. Its whether or not you have access to it
+
+--------------------------------------------------------------------------------------------
+
+TWO KINDS OF SCOPE
+
+1 - Local
+2 - Global
