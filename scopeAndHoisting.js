6b55e68d7919c90918c0a0d4b9d8285eee4fcea7
TKais <TiffanyTKaiser@gmail.com>
2014-11-08 15:24:55 -0800

    JavaScript has local ans global variable scopes

--- scopeAndHoisting.js
+++ scopeAndHoisting.js
@@ -1,6 +1,8 @@
-VARIABLE SCOPE
+--VARIABLE SCOPE--
 
-A variables scope is the context in which it exists. Its whether or not you have access to it
+*A variables scope is the context in which it exists. Its whether or not you have access to it
+*JavaScript does not have block-level scope, which means variables scoped to surrounding curly brackets.
+*JavaScript has function-level scope instead, where variables declared in a function are local variables and they can only be accessed within that function and by functions in that function.
 
 --------------------------------------------------------------------------------------------
 
@@ -8,3 +10,7 @@ TWO KINDS OF SCOPE
 
 1 - Local
 2 - Global
+
+LOCAL SCOPE
+Where variables declared in a function are local variables and they can only be accessed within that function and by functions in that function.
+
