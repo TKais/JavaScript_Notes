61396561d8218e17c2216bae13f5aed1296d1638
TKais <TiffanyTKaiser@gmail.com>
2014-11-08 16:18:22 -0800

    for loops in the global scope have global variables

--- scopeAndHoisting.js
+++ scopeAndHoisting.js
@@ -115,7 +115,18 @@ EX)
 ​// The second declaration of firstName simply re-declares and overwrites the first one​
 console.log (firstName); // Bob
 
+*For loops outside of a function have global variables too
+EX)
+for (var i = 1; i <= 10; i++) {
+  console.log (i); // outputs 1, 2, 3, 4, 5, 6, 7, 8, 9, 10;​
+};
+​// The variable i is a global variable and it is accessible in the following function with the last value it was assigned above ​
+​function aNumber () {
+console.log(i);
+}
+​// The variable i in the aNumber function below is the global variable i that was changed in the for loop above. Its last value was 11, set just before the for loop exited:​
+aNumber ();  // 11​
 
-
+-------------------------------------------------------------------------------------------
 
 
