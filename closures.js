aea7e274ec2c7c959bae3073ca293c856c7873a1
TKais <TiffanyTKaiser@gmail.com>
2014-11-09 17:51:40 -0800

    Closures see updated variables before even being invoked, which is why we use IIFEs

--- closures.js
+++ closures.js
@@ -46,9 +46,54 @@ var myName = showName("Tiff"); //shows the function layout
 myName("Kaiser"); //My name is Tiff Kaiser
 
 
-2) 
+2) Closures store references to the outer functions variables, not the actual value. Closures have access to updated values in its outer functions, so as those change, so do the values in the closures. 
+
+EX)
+function celebrityID () {
+    var celebrityID = 999;
+    // We are returning an object with some inner functions​
+    // All the inner functions have access to the outer function's variables​
+    return {
+        getID: function ()  {
+            // This inner function will return the UPDATED celebrityID variable​
+            // It will return the current value of celebrityID, even after the changeTheID function changes it​
+          return celebrityID;
+        },
+        setID: function (theNewID)  {
+            // This inner function will change the outer function's variable anytime​
+            celebrityID = theNewID;
+        }
+    }
+​
+}
+​
+​var mjID = celebrityID (); // At this juncture, the celebrityID outer function has returned.​
+mjID.getID(); // 999​
+mjID.setID(567); // Changes the outer function's variable​
+mjID.getID(); // 567: It returns the updated celebrityId variable 
 
 
+3) IIFE (Immediately Invoked Function Expression)
+A big issue with closures can be that they access outer function variables or global variables that changed before even being invoked. Like this: 
+
+EX)
+function celebrityIDCreator (theCelebrities) {
+    var i;
+    var uniqueID = 100;
+    for (i = 0; i < theCelebrities.length; i++) {
+      theCelebrities[i]["id"] = function ()  {
+        return uniqueID + i;
+      }
+    }
+    
+    return theCelebrities;
+}
+​
+​var actionCelebs = [{name:"Stallone", id:0}, {name:"Cruise", id:0}, {name:"Willis", id:0}];
+​
+​var createIdForActionCelebs = celebrityIDCreator (actionCelebs);
+​
+​var stalloneID = createIdForActionCelebs [0];  console.log(stalloneID.id()); // 103
 
 
 
