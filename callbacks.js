9875e56fcdf04dc7d3c33ce47fa029021c6aaf49
TKais <TiffanyTKaiser@gmail.com>
2014-11-10 12:51:11 -0800

    Callbacks are functions passed as arguments to other functions for asynchronou,s non-blocking purposes

---
+++ callbacks.js
@@ -0,0 +1,3 @@
+--CALLBACKS--
+
+A callback function, also known as a higher-order function, is a function that is passed to another function (let’s call this other function “otherFunction”) as a parameter, and the callback function is called (or executed) inside the otherFunction
