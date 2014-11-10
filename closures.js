361eaa876925adfeee2295f2a9de63bdad502c2f
TKais <TiffanyTKaiser@gmail.com>
2014-11-09 17:29:32 -0800

    closures have access to the outer function's variable even after the outer function returns

--- closures.js
+++ closures.js
@@ -13,3 +13,48 @@ function showName(firstName, lastName){
 
 showName("Tiff", "Kaiser"); //Your name is Tiff Kaiser
 
+------------------------------------------------------------------------------------------
+
+JQUERY
+
+*Closures are a major part of jQuery as well.
+
+EX)
+$(function() {
+  var selections = [];
+  $(".nines").click(function(){ // this closure has access to the selections variable
+    selections.push(this.prop("name")); // update the selections variable in the outer function's scope​
+  });
+});
+
+------------------------------------------------------------------------------------------
+
+CLOSURE RULES
+
+1) Closures have access to the outer function’s variable even after the outer function returns, meaning that even after the outer function has returned, the inner function still has access to the outer function’s variables -- and you can call them later in the program.
+
+EX)
+function showName(firstName){
+  var nameIntro = "My name is ";
+  function fullName(lastName){
+    return nameIntro + firstName + " " + lastName;
+  }
+  return fullName;
+}
+
+var myName = showName("Tiff"); //shows the function layout
+myName("Kaiser"); //My name is Tiff Kaiser
+
+
+2) 
+
+
+
+
+
+
+
+
+
+
+
