d7908a6c1e5a0d9003be2a5919b20970a7d7e19c
TKais <TiffanyTKaiser@gmail.com>
2014-11-10 20:40:47 -0800

    Callback functions are not executed immediately

--- callbacks.js
+++ callbacks.js
@@ -1,3 +1,14 @@
 --CALLBACKS--
 
 A callback function, also known as a higher-order function, is a function that is passed to another function (let’s call this other function “otherFunction”) as a parameter, and the callback function is called (or executed) inside the otherFunction
+
+*We can pass functions around like variables and return them in other functions.
+
+------------------------------------------------------------------------
+
+HOW CALLBACKS WORK
+
+*When we pass a callback function as an argument to another function, we are only passing the function definition. We are not executing the function in the parameter. In other words, we aren’t passing the function with the trailing pair of executing parenthesis () like we do when we are executing a function
+
+*since the containing function has the callback function in its parameter as a function definition, it can execute the callback anytime. IT IS NOT EXECUTED IMMEDIATELY.
+
