94f8a196cb6fb03750734cd87421d971db0021fa
TKais <TiffanyTKaiser@gmail.com>
2014-11-13 14:17:12 -0800

    An intro to 'this' and an analogy for help

---
+++ this.js
@@ -0,0 +1,12 @@
+--THIS--
+
+*References an object (the subject in context, or the subject of the executing code)
+*Think of 'this' as a pronoun that refers to a noun.
+
+EX) 
+"Tiff is running fast because she is trying to catch a plane"
+'Tiff' is the object, and 'he' is the 'this'. 
+ELSE
+"Tiff is running fast because Tiff is trying to catch a plane"
+//too redundant, we need pronouns. And we need 'this'
+
