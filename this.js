bed3cf7a2cc80f907ae30bdb7e585be69011397b
TKais <TiffanyTKaiser@gmail.com>
2014-11-15 21:15:44 -0800

    Used bind to attach 'this' to an object when a method is assigned to a variable

--- this.js
+++ this.js
@@ -162,9 +162,45 @@ var user = {
 
   //we've set the 'this' to a variable before entering the each loop (or in any case, the closure)
 
+----------------------------------------------------------------------------------
 
+USING 'THIS' WHEN METHOD IS ASSIGNED TO A VARIABLE 
 
+EX)
+// This data variable is a global variable​
+    var data = [
+    {name:"Samantha", age:12},
+    {name:"Alexis", age:14}
+    ];
+​
+    var user = {
+    // this data variable is a property on the user object​
+    data:[
+    {name:"T. Woods", age:37},
+    {name:"P. Mickelson", age:43}
+    ],
+    showData:function (event) {
+    var randomNum = ((Math.random () * 2 | 0) + 1) - 1; // random number between 0 and 1​
+​
+    // This line is adding a random person from the data array to the text field​
+    console.log (this.data[randomNum].name + " " + this.data[randomNum].age);
+    }
+​
+    }
+​
+    // Assign the user.showData to a variable​
+    var showUserData = user.showData;
+​
+    // When we execute the showUserData function, the values printed to the console are from the global data array, not from the data array in the user object​
+    //​
+    showUserData(); // Samantha 12 (from the global data array)​
 
+//Do this instead -------------------------------------
+var showUserData = user.showData.bind(user);
+showUserData (); // P. Mickelson 43
+//used bind again to attach it to the correct object -- not the global object
+
+----------------------------------------------------------------------------------
 
 
 
