22dc0548ed8905b06bc03c5cf4c88aa26a4c66d7
TKais <TiffanyTKaiser@gmail.com>
2014-11-15 21:51:27 -0800

    Use apply to properly set 'this' when borrowing methods

--- this.js
+++ this.js
@@ -202,6 +202,49 @@ showUserData (); // P. Mickelson 43
 
 ----------------------------------------------------------------------------------
 
+USING 'THIS' WHEN BORROWING METHODS 
+
+EX) 
+// We have two objects. One of them has a method called avg () that the other doesn't have​
+    // So we will borrow the (avg()) method​
+    var gameController = {
+    scores: [20, 34, 55, 46, 77],
+    avgScore: null,
+    players:[
+    {name:"Tommy", playerID:987, age:23},
+    {name:"Pau", playerID:87, age:33}
+    ]
+    }
+​
+    var appController = {
+    scores  :[900, 845, 809, 950],
+    avgScore: null,
+    avg: function(){
+    var sumOfScores = this.scores.reduce(function(prev, cur, index, array){
+    return prev + cur;
+    });
+    this.avgScore = sumOfScores / this.scores.length;
+    }
+    }
+​
+    //If we run the code below,​
+    // the gameController.avgScore property will be set to the average score from the appController object "scores" array​
+   
+    // Don't run this code, this is just for illustration; we want the appController.avgScore to remain null​
+    gameController.avgScore = appController.avg();
+​
+
+//Do this instead------------------------------------------
+appController.avg.apply (gameController, gameController.scores);
+//we use the apply method because the second argument needs to be an array
+//The first parameter in the apply method always sets the value of “this” explicitly.
+
+
+
+console.log (gameController.avgScore); // 46.4
+console.log (appController.avgScore); // null
+
+
 
 
 
