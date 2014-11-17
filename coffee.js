146c23e4e5df6a66f56a69fec15d2ac4fea0c6d4
TKais <TiffanyTKaiser@gmail.com>
2014-11-16 21:10:51 -0800

    Coffeescript example

--- coffee.js
+++ coffee.js
@@ -22,3 +22,42 @@ foobar(function() {
     alert(foo.array);
 });
 //foobar is called and once 1000 milliseconds goes by, the number 1 is alerted. 
+
+
+
+
+
+--COFFEESCRIPT EXAMPLE--
+
+foobar = (callback) ->
+  setTimeout (->
+    
+    # trigger callback after 1000ms
+    callback()
+    return
+  ), 1000
+  return
+
+foo =
+  key:
+    nestedKey: "value"
+
+  array: [1]
+  nestedArray: [
+    1
+    2
+    [
+      "2a"
+      ["2a-I"]
+    ]
+  ]
+
+foobar ->
+  alert foo.array
+  return
+
+---------------------------------------------------------------------------------
+
+COFFEESCRIPT BASICS
+
+()-> //this is a function
