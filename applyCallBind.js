0d3c2f9a56416fd05e8d79bdcd316292180b3735
TKais <TiffanyTKaiser@gmail.com>
2014-11-16 19:45:13 -0800

    how apply, call and bind are most commonly used

---
+++ applyCallBind.js
@@ -0,0 +1,12 @@
+--APPLY, CALL & BIND --
+
+*Functions are objects, and have methods --including bind, call and apply. 
+
+*Apply and call are nearly identical, except apply takes an array as the second argument. They are both used for setting the 'this' value explicitly, and borrowing methods.
+
+*Bind is used to set the 'this' value in methods.
+
+-------------------------------------------------------------------------------
+
+BIND
+
