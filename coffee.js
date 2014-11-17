84d0c8a179dde59c5b72592ebd1f1777f6c575da
TKais <TiffanyTKaiser@gmail.com>
2014-11-16 21:56:29 -0800

    Using default parameters in coffeescript

--- coffee.js
+++ coffee.js
@@ -72,3 +72,53 @@ coffee = (message, other) -> //is the same as
 coffee("Yo", 2)
 
 *Message and Other above are both parameters being passed to the function
+
+---------------------------------------------------------------------------------
+
+USING DEFAULT PARAMETERS 
+
+coffee = (message = "Ready for some Coffee?") ->
+  answer = confirm message
+  "Your answer is #{answer}"
+
+alert coffee() //Ready for some Coffee?
+alert coffee("Want some decaf?") //Want some decaf?
+
+
+//It compiles into this JavaScript...
+
+var coffee;
+coffee = function(message){
+    var answer;
+    if(message == null){
+        message = "Ready for some Coffee?";
+    }
+    answer = confirm(message);
+    return "Your answer is " + answer;
+}
+
+---------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
