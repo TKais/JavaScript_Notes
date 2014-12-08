7e8cc09fc5bc2628828ecc425c0afec9a49e24e8
TKais <TiffanyTKaiser@gmail.com>
2014-12-08 12:53:47 -0800

    Always add view and model instance variables as arguments to controller constructor object

--- MVC.js
+++ MVC.js
@@ -14,3 +14,8 @@
     1) variables to represent model, view and controller objects (EX, var view = new View(), var controller = new Controller() and var model = new Model())
     2) controller.bindListeners() to load the controllers actions (these attach the view variables (representatives of the html classes or IDs) to the controllers methods)
 2) Each controller method should have an action that interacts with the model and another that interacts with the view. It switches between them. Use one model method against one view method.
+3) Controller MUST have view and model variables passed to it as two different arguments when creating the Controller constructor object (EX:
+  function Controller(view, model){
+    this.view = view;
+    this.model = model;
+  })
