275076104fd213016908edcaac765afe4bfee01a
TKais <TiffanyTKaiser@gmail.com>
2014-12-25 22:07:34 -0500

    Add notes about the use of directives in angular.js

--- angular.js
+++ angular.js
@@ -1,3 +1,5 @@
 --ANGULAR--
 
 A JavaScript framework that allows you to quickly develop a website by extending HTML vocabulary for dynamic views. 
+
+1) Uses directives in the html to add angular elements, such as proper date formatting, proper currency formatting, etc.
