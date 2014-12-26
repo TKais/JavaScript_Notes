1b50c7efd5797094f4b52400265c20a5852f190e
TKais <TiffanyTKaiser@gmail.com>
2014-12-26 11:14:27 -0500

    Notes on angular directives syntax

--- angular.js
+++ angular.js
@@ -3,3 +3,5 @@
 A JavaScript framework that allows you to quickly develop a website by extending HTML vocabulary for dynamic views. 
 
 1) Uses directives in the html to add angular elements, such as proper date formatting, proper currency formatting, etc.
+
+2) Directives start with 'ng' + hyphen + whatever the directive is called. For instance, to make your app an angular app, you have to have 'ng-app = [name of app]'
