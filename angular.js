74b94c8e7f81dd83750742aeb9f6123506fb7952
TKais <TiffanyTKaiser@gmail.com>
2014-12-31 18:13:51 -0500

    More dorectives in angular

--- angular.js
+++ angular.js
@@ -5,3 +5,4 @@ A JavaScript framework that allows you to quickly develop a website by extending
 1) Uses directives in the html to add angular elements, such as proper date formatting, proper currency formatting, etc.
 
 2) Directives start with 'ng' + hyphen + whatever the directive is called. For instance, to make your app an angular app, you have to have 'ng-app = [name of app]'
+  Other directives look like ng-currency and ng-controller for the controller and ng-repeat for loops. 
