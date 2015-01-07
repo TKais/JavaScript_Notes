74845695070873816066ffdaa4ad9eb50e37c360
TKais <TiffanyTKaiser@gmail.com>
2015-01-07 10:13:21 -0800

    definition and example of an endpoint in an API

--- api.js
+++ api.js
@@ -13,3 +13,7 @@ An HTTP Request is made up of:
   1. The request line (tells the server what kind of request is being sent (GET, POST, etc.) and what resource its looking for)
   2. The header (sends the server additional information (such as which client is making the request))
   3. The body can be empty (as in a GET request) or contain data (if youre POSTing or PUTing information, that information is contained here).
+
+
+ENDPOINTS = API-defined locations where particular data are stored. 
+     EX) Just as youll GET a pair of pants from PantsWorld or youll GET a bag of peanuts from PeanutHut, youll GET something different depending on the endpoint you use
