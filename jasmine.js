5268c7208b04fdf41ddd2bb17668a8512fed0893
TKais <TiffanyTKaiser@gmail.com>
2015-01-13 13:24:28 -0800

    Some included matcher examples

--- jasmine.js
+++ jasmine.js
@@ -32,3 +32,92 @@ MATCHERS:
 
 Implements a boolean comparison between the actual value and the expected value. 
 
+Some included matchers:
+1 - expect(true).toBe(true)
+2 - expect(false).not.toBe(true);
+3 - var a = 12;
+      expect(a).toEqual(12);
+4 - var foo = {
+        a: 12,
+        b: 34
+      };
+      var bar = {
+        a: 12,
+        b: 34
+      };
+      expect(foo).toEqual(bar);
+5 - expect(message).toMatch(/bar/);
+6 - var a = {
+      foo: "foo"
+    };
+
+    expect(a.foo).toBeDefined();
+    expect(a.bar).not.toBeDefined();
+7 - var a = null;
+    var foo = "foo";
+
+    expect(null).toBeNull();
+    expect(a).toBeNull();
+    expect(foo).not.toBeNull();
+8 - var a, foo = "foo";
+
+    expect(foo).toBeTruthy();
+    expect(a).not.toBeTruthy();
+9 - var a, foo = "foo";
+
+    expect(a).toBeFalsy();
+    expect(foo).not.toBeFalsy();
+10 - var a = ["foo", "bar", "baz"];
+
+    expect(a).toContain("bar");
+    expect(a).not.toContain("quux");
+11 - var pi = 3.1415926,
+      e = 2.78;
+
+    expect(e).toBeLessThan(pi);
+    expect(pi).not.toBeLessThan(e);
+12 - var pi = 3.1415926,
+      e = 2.78;
+
+    expect(pi).toBeGreaterThan(e);
+    expect(e).not.toBeGreaterThan(pi);
+13 - var pi = 3.1415926,
+      e = 2.78;
+
+    expect(pi).not.toBeCloseTo(e, 2);
+    expect(pi).toBeCloseTo(e, 0);
+14 - var foo = function() {
+      return 1 + 2;
+    };
+    var bar = function() {
+      return a + 1;
+    };
+
+    expect(foo).not.toThrow();
+    expect(bar).toThrow();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
