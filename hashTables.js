0e67ce9ad021e676e8713c384c164c93f963d2bd
TKais <TiffanyTKaiser@gmail.com>
2015-02-20 13:31:06 -0800

    Defining and showing simple example of a hash table in JavaScript

---
+++ hashTables.js
@@ -0,0 +1,9 @@
+// A hash table is just an associative array with key/value pairs, much like a hash in Ruby. All non-scalar objects in JavaScript act like associative array (hash tables) and look like this:
+
+var hashTable = {};
+
+hashTable['key1'] = 1;
+hashTable['key2'] = 2;
+hashTable['key3'] = 3;
+
+console.log(hashTable); // { key1: 1, key2: 2, key3: 3 }
