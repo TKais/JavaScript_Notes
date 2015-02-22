962f0215e01e04e540bacfe7e4480a0db2f1c5a8
TKais <TiffanyTKaiser@gmail.com>
2015-02-21 19:47:37 -0800

    Outputting the values in a hash table

--- hashTables.js
+++ hashTables.js
@@ -7,3 +7,21 @@ hashTable['key2'] = 2;
 hashTable['key3'] = 3;
 
 console.log(hashTable); // { key1: 1, key2: 2, key3: 3 }
+
+
+//Show the values inside
+
+var hashTable = {};
+
+hashTable['key1'] = 1;
+hashTable['key2'] = 2;
+hashTable['key3'] = 3;
+
+for(var x in hashTable){
+  if(hashTable.hasOwnProperty(x)){ //use hasOwnProperty to filter out keys from Object.prototype
+    console.log('key is: ' + x + '. and the value is: ' + hashTable[x]);
+  }
+}
+
+
+//Cannot use the length() function with hash tables because it's not indexed like arrays
