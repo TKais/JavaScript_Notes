89fc2d51102cd55502fe72590fcfcca5a1169e32
TKais <TiffanyTKaiser@gmail.com>
2015-02-27 16:36:09 -0800

    Definition of a binary tree and a simple JS example

---
+++ binaryTrees.js
@@ -0,0 +1,8 @@
+//A binary search tree is a doubly linked list that contains some data and pointers to two nodes. A binary search tree node’s pointers are typically called “left” and “right” to indicate subtrees of values relating to the current value. A simple JavaScript implementation of such a node is as follows:
+
+
+var node = {
+  value: 125,
+  left: null,
+  right: null
+}
